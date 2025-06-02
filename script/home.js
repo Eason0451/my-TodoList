9678a7f4108da53f6910f434a52fea3d2c7dac5e
Eason <a0983056919@gmail.com>
2025-06-02 15:37:59 +0800

    id 錯誤修復完畢

--- script/home.js
+++ script/home.js
@@ -1,4 +1,4 @@
-import { todoList,addTodoList,deletTodoItem,finishTodoItem,addCategoryName,deleCategory} from "./data/todo-lsit.js";
+import { todoList,addTodoList,deletTodoItem,finishTodoItem,addCategoryName,deleCategory,reset} from "./data/todo-lsit.js";
 selectOption();
 titleHtml();
 finishTodoHtml();
