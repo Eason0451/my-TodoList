12b17af04d9ffba134e429da1549907761e9d9b0
Eason <a0983056919@gmail.com>
2025-06-16 18:27:16 +0800

    修復日期顯示2

--- script/home.js
+++ script/home.js
@@ -253,8 +253,8 @@ function todoHtml(array){
                         type="checkbox"
                     > 
                     <${todo.done? "del":"span"} class="todo-name">
-                        <span class="todo-text">${todo.name}</span>
-                        ${todo.date? `<span class="dateName item-name ${dateColor(todo)}">${todo.done? "<del>"+dateString(todo)+"</del>":dateString(todo)}</span>`:"" }
+                        ${todo.name}
+                        ${todo.date? `<span class="dateName item-name ${dateColor(todo)}">${dateString(todo)}</span>`:"" }
                     </${todo.done? "del":"span"}> 
                    
                     <button 
